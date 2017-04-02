92a18912d07f781174955f24f1217e0e550f1529
Roberto Ambrosini <ambrosini.roberto@gmail.com>
2017-04-01 22:50:09 -0500

    luego de login redirecciono al listado de usuarios

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -10,7 +10,7 @@ defmodule Rumbl.SessionController do
       {:ok, conn} ->
         conn
         |> put_flash(:info, "Welcome back!")
-        |> redirect(to: page_path(conn, :index))
+        |> redirect(to: user_path(conn, :index))
       {:error, _reason, conn} ->
         conn
         |> put_flash(:error, "Invalid username/password combination")
