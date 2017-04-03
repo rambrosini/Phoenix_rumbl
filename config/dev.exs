5c36b88f825882e1b03579e6f024968d85207db4
Roberto Ambrosini <ambrosini.roberto@gmail.com>
2017-04-02 20:40:03 -0500

    Videos resources creados

--- config/dev.exs
+++ config/dev.exs
@@ -36,7 +36,7 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "Roberto",
+  username: "roberto",
   password: "",
   database: "rumbl_dev",
   hostname: "localhost",
